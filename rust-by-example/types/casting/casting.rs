3f2988c5ce4557d584ff8fbb28fefc054f06d4e7
Richard Downer <richard.downer@xero.com>
2014-08-06 13:11:10 +1200

    casting

---
+++ rust-by-example/types/casting/casting.rs
@@ -0,0 +1,13 @@
+fn main() {
+	
+	let decimal = 65.4321_f32;
+
+	// Error! no implicit conversion
+	// let integer: u8 = decimal;
+
+	// Explicit conversion
+	let integer = decimal as u8;
+	let character = integer as char;
+	println!("Casting: {} -> {} -> {}", decimal, integer, character);
+
+}
