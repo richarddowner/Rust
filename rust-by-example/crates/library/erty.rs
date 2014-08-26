141502b60d1dee8a06dac4b49aeeac1356c1a991
Richard Downer <richarddowna@gmail.com>
2014-08-27 07:31:27 +1200

    crate library erty

---
+++ rust-by-example/crates/library/erty.rs
@@ -0,0 +1,14 @@
+// erty.rs
+pub fn public_function() {
+	println!("called erty's `public_function()`");
+}
+
+fn private_function() {
+	println!("called erty's `private_function()`");
+}
+
+pub fn indirect_access() {
+	println!("called erty's `indirect_access()`, that\n> ");
+
+	private_function();
+}
